14f7965d0b68388daed0597e30fcac5c70831912
GoHann <975379844@qq.com>
2023-02-01 15:57:38 +0800

    Setup basic Application and Entry Point.

---
+++ Hazel/src/Hazel/Application.cpp
@@ -0,0 +1,21 @@
+#include "Application.h"
+
+
+namespace Hazel{
+
+	Application::Application()
+	{
+	}
+
+	Application::~Application()
+	{
+	}
+
+	void Application::Run()
+	{
+		while (true); //一个无限循环的应用程序
+
+	}
+
+}
+
